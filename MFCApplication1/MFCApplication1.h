1a69a1fbd1232eeb76fb4ed71c83868acea3536a
wacpt4 <675135130@qq.com>
2020-05-19 21:19:51 +0800

    添加项目文件。

---
+++ MFCApplication1/MFCApplication1.h
@@ -0,0 +1,40 @@
+﻿
+// MFCApplication1.h: MFCApplication1 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMFCApplication1App:
+// 有关此类的实现，请参阅 MFCApplication1.cpp
+//
+
+class CMFCApplication1App : public CWinAppEx
+{
+public:
+	CMFCApplication1App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication1App theApp;
